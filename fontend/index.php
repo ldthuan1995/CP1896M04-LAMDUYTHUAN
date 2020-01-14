3c6052740ea93cae51f08d5e70c273ad4309ce15
ldthuan1995 <44872705+ldthuan1995@users.noreply.github.com>
2020-01-14 21:32:09 +0700

    boostrap

---
+++ fontend/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/CP1896M04-LAMDUYTHUAN/assets/vendor/bootstrap/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+    div{
+      border : solid 1px red ;
+    }
+    </style>
+</head>
+<body>
+    <div class ="container">
+        <div class = "row">
+            <div class ="col-md-3">
+            Logo
+            </div>
+            <div class ="col-md-9">
+            Cong ty TNHH...
+            </div>
+        </div> <!--header-->
+
+        <div class ="row">
+            <div class ="col-md-12">
+            MENU
+            </div>
+        </div> <!-- MENU-->
+
+        <div class ="row">
+        <div class ="col-md-8">
+        ...
+        </div>
+        <div class ="col-md-4">
+        ...
+        </div>
+        
+        </div>
+
+
+    </div>   
+
+</body>
+</html>
